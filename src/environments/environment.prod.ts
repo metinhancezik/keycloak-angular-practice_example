9c7b32fb9df89f0cb607874235b6dec0f7f4083e
Metinhan Çezik <metinhansoftware@gmail.com>
2024-02-16 15:04:46 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  appUrl: 'http://localhost:4200',
+  appUrl: 'http://localhost:4200/',
   keycloakConfig: {
-    url: 'http://localhost:8080/auth/',
+    url: 'http://localhost:8080/',
     realm: 'sample-realm',
     clientId: 'angular-app',
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appUrl: 'http://localhost:4200/',
   keycloakConfig: {
-    url: 'http://localhost:8080/auth/',
+    url: 'http://localhost:8080/',
     realm: 'sample-realm',
     clientId: 'angular-app',
   },
